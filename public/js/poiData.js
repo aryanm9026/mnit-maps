1aade538638380781d10907dd5b38f3e26d1d39a
Sushobhit-Katiyar <sushobhitkatiyar@gmail.com>
2025-08-15 23:59:27 +0530

    Added poi from pois.geojson in poiData.js

--- public/js/poiData.js
+++ public/js/poiData.js
@@ -20,5 +20,462 @@ export const poiList = [
     category: "Cafes",
     description: "Popular spot for snacks & coffee.",
     timings: "8:00 AM - 10:00 PM"
+  },
+  // Sushobhit added after that 
+  {
+    name: "H-16",
+    coords: [26.8621268, 75.8209989],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "FFR Lab",
+    coords: [26.8615779, 75.8108139],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Foundary Technology",
+    coords: [26.8617525, 75.8106584],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Running Ground",
+    coords: [26.8601663, 75.8147214],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Mineral Processing Lab",
+    coords: [26.8617095, 75.8107818],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Earthquake Detection Center",
+    coords: [26.8604912, 75.8091911],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "MIIC",
+    coords: [26.8605508, 75.808623],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Computer Science Department",
+    coords: [26.8627913, 75.8106962],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Gargi Gate",
+    coords: [26.8648285, 75.8139369],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Alumini Association",
+    coords: [26.8595775, 75.8107439],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Architecture Department",
+    coords: [26.8621696, 75.8111066],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Chemical Department",
+    coords: [26.8624373, 75.8102858],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Metallurgical and Materials Engineering Department",
+    coords: [26.8625396, 75.8099127],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "ECE department",
+    coords: [26.8627348, 75.8116603],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Central Canteen",
+    coords: [26.8631305, 75.8105471],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "VLTC back porch",
+    coords: [26.862502, 75.8143449],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Material Research Center",
+    coords: [26.8615842, 75.8112243],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Central Store",
+    coords: [26.8602402, 75.8106928],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Mechanical Workshop",
+    coords: [26.8608691, 75.8103522],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "3D Printing Lab",
+    coords: [26.8619926, 75.810631],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Chemistry Department",
+    coords: [26.8613295, 75.8098776],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Humanities Department",
+    coords: [26.8614715, 75.8102217],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Chemistry Department",
+    coords: [26.8617959, 75.8112905],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Electrical Department",
+    coords: [26.8615696, 75.8104148],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Service Gate",
+    coords: [26.8625295, 75.8229561],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "OAT",
+    coords: [26.8619364, 75.8118249],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Basketball court",
+    coords: [26.8614914, 75.814669],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Dean's Gate",
+    coords: [26.8588652, 75.8104998],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Back Gate",
+    coords: [26.8569032, 75.8168452],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Main Gate",
+    coords: [26.8650752, 75.8077545],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Staff Gate",
+    coords: [26.8671088, 75.8106799],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-6( Kabir Hostel)",
+    coords: [26.8605447, 75.8193854],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Sports Complex Gate",
+    coords: [26.8617185, 75.8137912],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Metallurgy Workshop",
+    coords: [26.861572, 75.8106106],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-8 ( Varun Hostel )",
+    coords: [26.8604325, 75.8193734],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-15 ( Aravali Hostel ) ",
+    coords: [26.8604214, 75.8205603],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-7 (Drona Hostel)",
+    coords: [26.8597372, 75.8183271],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-4 (Lohit Hostel)",
+    coords: [26.8600705, 75.8177745],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-5 (Brihaspati Hostel)",
+    coords: [26.8605576, 75.8177228],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-3 (Satpura Hostel)",
+    coords: [26.8606404, 75.815605],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Kabaddi Ground",
+    coords: [26.8600997, 75.8155967],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Hostel Office (Boys)",
+    coords: [26.8620473, 75.8159342],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Civil Department",
+    coords: [26.8611605, 75.8088278],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Library",
+    coords: [26.8621619, 75.8086806],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Management Department",
+    coords: [26.8625902, 75.8089435],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Nursery",
+    coords: [26.8620886, 75.8157424],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-1 (Parijat Hostel) ",
+    coords: [26.8623556, 75.816019],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-2 (Chaitanya Hostel)",
+    coords: [26.8618665, 75.8160122],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-10 (Kailash Hostel for PG)",
+    coords: [26.86309, 75.8156339],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Physics Department",
+    coords: [26.862613, 75.8116324],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Electronics and Communication Department",
+    coords: [26.8627944, 75.8117245],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "H-9 ( Aurobindo Hostel )",
+    coords: [26.8621487, 75.8200818],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Prabha Bhawan",
+    coords: [26.8637887, 75.8108025],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Mathematics department",
+    coords: [26.8626854, 75.8144156],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Vinodhini gate",
+    coords: [26.8636959, 75.8155781],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Choti Lab",
+    coords: [26.8620003, 75.8104664],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Thadi gate",
+    coords: [26.8590493, 75.8155132],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Loondry",
+    coords: [26.8608893, 75.8173379],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Materials Testing Lab",
+    coords: [26.8620333, 75.8101466],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Night Canteen",
+    coords: [26.8597961, 75.8178562],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "FootBall Ground",
+    coords: [26.8600852, 75.8147902],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Box Cricket",
+    coords: [26.8593604, 75.8143569],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Cricket Ground",
+    coords: [26.8615576, 75.8136685],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Hair Cutting Salon",
+    coords: [26.8608601, 75.8154706],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
+  },
+  {
+    name: "Basketball Court (Aurobindo)",
+    coords: [26.8616835, 75.8202036],
+    category: "General",
+    description: "No description available.",
+    timings: "Not specified"
   }
+
 ];
